6720d1bc80cb2ba7ff8e1045f869f1969f447131
abhinav3254 <abhinav3254@gmail.com>
2025-02-04 21:32:12 +0530

    removed old files

--- src/constants/Skills.ts
+++
@@ -1,4 +0,0 @@
-export const SkillsArray = ["Android", "Angular", "Azure", "CSS", "Flask", "Flutter", "GIT",
-    "Github", "Hibernate", "HTML", "Ionic", "Java", "Javascript", "Linux", "MySQL",
-    "Node", "Postgresql", "Python", "Reactjs", "Spring", "Socket", "Tailwind", "Typescript"
-]
