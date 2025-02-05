7659475b632c56cb3f057f0ee2cbfb9b6eabdb0f
abhinav3254 <abhinav3254@gmail.com>
2025-02-05 23:23:09 +0530

    user's input

--- investment/investment_calculator.go
+++ investment/investment_calculator.go
@@ -7,11 +7,33 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	// var investmentAmount float64 = 1000
+	// var expectedReturnRate = 5.5 -- this also be written as bottom
+	// expectedReturnRate := 5.5
+	// var years float64 = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	// How declare multiple values in one single line
+	// This approach makes the code hard to read because we have to make a close look to check which
+	// value belong to which variable
+	// investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+
+	const inflationRate = 2.5
+
+	investmentAmount := 1000.0
+	years := 10.0
+	expectedReturnRate := 5.5
+
+	// now taking user's input
+	// Here & is the pointer for the investmentAmount
+	// we can't directly pass investmentAmount instead we pass pointer of that variable
+	fmt.Scan(&investmentAmount)
+
+	// We can also keep different kind of data types like this by not defining the float64
+	// var investmentAmount, years = 1000, "10"
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
